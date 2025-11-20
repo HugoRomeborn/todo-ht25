c1c00ff75f6753649b8ba9793ed2c83ae2e1cc0a
Romebomben <hugo.romeborn@elev.ga.ntig.se>
2025-11-20 16:41:49 +0100

    Add todo creation and update view
    Implemented a form and route to create new todos, saving them to the database. Added a new update view and route to display a single todo for updating. Updated the index view to list all todos and show the creation form.

--- app.rb
+++ app.rb
@@ -5,11 +5,38 @@ require 'sinatra/reloader'
 
 
 
+post '/create' do
+    name = params["name"]
+    description = params["description"]
+    db = SQLite3::Database.new("db/todos.db")
 
+    db.execute("INSERT INTO todos (name, description) VALUES (?,?)", [name, description])
+    redirect('/')
+end
 
-# Routen /
+# Routen 
 get '/' do
+    #Koppling till db
+    db = SQLite3::Database.new("db/todos.db")
+    db.results_as_hash = true
+
+    #hämta från db
+    @todos = db.execute("SELECT * FROM todos")
+
+    p @datafrukt
+    #visa med slim
     slim(:index)
+end
+
+get '/:id/update' do
+    id = params[:id].to_i
+
+    db = SQLite3::Database.new("db/fruits.db")
+    db.results_as_hash = true
+    @todo = db.execute("SELECT * FROM todos WHERE id = ?",id).first
+
+
+    slim(:update)
 end
 
 
